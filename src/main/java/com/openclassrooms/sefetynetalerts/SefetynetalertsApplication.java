239dbbfee863359bb0edf344898b0315d226e00d
GarlandNic <nicolas-garland@club-internet.fr>
2023-02-24 14:52:38 +0100

    Add files via upload

---
+++ src/main/java/com/openclassrooms/sefetynetalerts/SefetynetalertsApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.sefetynetalerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SefetynetalertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SefetynetalertsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openclassrooms/sefetynetalerts/SefetynetalertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.sefetynetalerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SefetynetalertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
